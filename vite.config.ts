87c94e52a44a9a12b87b0ce8e9b6c23ecdabdd4b
DochertyDev <work.spectrum532@passmail.com>
2025-12-19 20:36:12 -0400

    First Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,12 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    // Load environment variables based on the current mode
-    // Second parameter is the directory to search for .env files.
-    // Third parameter is the prefix for environment variables to be loaded.
-    const env = loadEnv(mode, process.cwd(), '');
-
-    return {
-      // Conditionally set the base path
-      base: mode === 'development' ? '/' : '/ScriptureSteps/',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        // Expose environment variables to your client-side code
-        // e.g., import.meta.env.VITE_SOME_KEY
-        'process.env': env
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Ensures assets are loaded correctly on GitHub Pages subpaths
+  build: {
+    outDir: 'dist',
+  }
 });
